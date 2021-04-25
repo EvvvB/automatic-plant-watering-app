c7b1b39edc37fcfc3826c8cf5ae3167fd5dfdf46
evvvb <ballesteros.evan@gmail.com>
2021-04-24 17:53:04 -0700

    React2

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const express = require('express')
 const cors = require('cors')
 // Create the server
 const app = express()
-
+//path for serving REACT from node requests
+const path = require('path')
 
 app.use(cors())
 // Choose the port and start the server
 
-app.get("/",(req, res)=>{
+app.get("/test",(req, res)=>{
     res.send("works!");
 })
 
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
